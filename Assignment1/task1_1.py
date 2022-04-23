c0ab3b01d3060439e207111eabc430d6882e5020
taliBerman5 <taliberman5@gmail.com>
2022-04-23 11:51:38 +0300

    fixed

--- Assignment1/task1_1.py
+++ Assignment1/task1_1.py
@@ -20,58 +20,50 @@ def eta_calc(X, W):
 def softmax(X, W):
     eta = eta_calc(X, W)
     return np.exp((X.T @ W).T - eta) / np.sum(np.exp((X.T @ W).T - eta).T,
-                                              axis=1)  # mabye needs transpose after subtruct eta
+                                              axis=1)
 
 
 def softmax_regression(C, X, W):
     n, m = np.shape(X)
-    return -1 / m * np.sum(C * np.log(softmax(X, W)))
+    return -1 / m * np.sum(C * np.log(softmax(X, W)).T)
 
 
 def softmax_regression_grad(C, X, W):
     n, m = np.shape(X)
-    gradf = X @ (softmax(X, W) - C)  # TODO: need transpose?
+    gradf = X @ (softmax(X, W) - C.T).T
     return 1 / m * gradf
 
 
 def grad_test():
     X = Yt
-    C = Ct
+    C = Ct.T
     X = np.vstack((X, np.ones(len(X[0]))))  # added dimension for the bias
     epsilon = 0.1
     n = len(X)
     l = len(C[1])
     W = np.random.rand(n, l)
-    D = np.random.rand(n, len(X[0]))
+    D = np.random.rand(n, l)
     D = (1 / LA.norm(D)) * D  # normelize to 1
     F0 = softmax_regression(C, X, W)
     g0 = softmax_regression_grad(C, X, W)
     linearly_grad_test = []
     quadratically_grad_test = []
-    for k in range(1, 20):
-        Fk = softmax_regression(C, X + epsilon * D, W)
-        F1 = F0 + epsilon * (g0 @ D)
+    for k in range(20):
+        Fk = softmax_regression(C, X, W + epsilon * D)
+        F1 = F0 + epsilon * (np.ndarray.flatten(g0,'F') @ np.ndarray.flatten(D,'F'))
         linearly_grad_test.append(abs(Fk - F0))
         quadratically_grad_test.append(abs(Fk - F1))
         epsilon = epsilon * 0.5
 
     axis = [i for i in range(20)]
     plt.figure()
-    plt.semilogy(axis, linearly_grad_test, lable="Zero order approx")
-    plt.semilogy(axis, quadratically_grad_test, lable="First order approx")
+    plt.semilogy(axis, linearly_grad_test, label="Zero order approx")
+    plt.semilogy(axis, quadratically_grad_test, label="First order approx")
     plt.xlabel("k")
     plt.ylabel("error")
     plt.title("Grad test for softmax regression")
     plt.legend()
     plt.show()
 
-
 grad_test()
-# X = Yt
-# C = Ct
-# X = np.vstack((X, np.ones(len(X[0]))))  # added dimension for the bias
-#
-# a = np.asarray([[1, 2, 3], [10, 11, 12]])
-# print(a)
-# print()
-# print((a.T - np.asarray([1, 2])).T)
+

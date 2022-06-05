dc948f8c8c07b1f8ffbbf8e711a5fa9e10c780fb
taliBerman5 <taliberman5@gmail.com>
2022-06-05 12:09:57 +0300

    fixed data loader

--- Assignment2/MNIST/lstm_ae_mnist.py
+++ Assignment2/MNIST/lstm_ae_mnist.py
@@ -10,9 +10,9 @@ import math
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081))])
 trainset = torchvision.datasets.MNIST(root="../data/", train=True, download=True, transform=transform)
-trainloader = torch.utils.data.Dataloader(trainset, batch_size=100, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True)
 testset = torchvision.datasets.MNIST(root="../data/", train=False, download=True, transform=transform)
-testloader = torch.utils.data.Dataloader(testset, batch_size=len(testset), shuffle=False)
+testloader = torch.utils.data.DataLoader(testset, batch_size=len(testset), shuffle=False)
 
 batch = 100
 epochs = 20
--- Assignment2/workshop.py
+++ Assignment2/workshop.py
@@ -35,9 +35,9 @@ transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((.5, .5, .5), (.5, .5, .5))])  # gets data to [-1,1]
 
 trainset = torchvision.datasets.CIFAR10(root="../data/", train=True, download=True, transform=transform)
-trainloader = torch.utils.data.Dataloader(trainset, batch_size=4, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True)
 testset = torchvision.datasets.CIFAR10(root="../data/", train=False, download=True, transform=transform)
-testloader = torch.utils.data.Dataloader(testset, batch_size=4, shuffle=False)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False)
 
 dataiter = iter(trainloader)
 images, labels = dataiter.next()
